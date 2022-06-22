672fc255278165631971a6d0995fa3d63cd86a29
Fernando-Ibarra <fi94457@gmail.com>
2022-06-21 21:20:54 -0600

    Taller 1 terminado

--- figuras.js
+++ figuras.js
@@ -1,54 +1,101 @@
 // Código del Cuadrado
 console.group("Cuadrado");
 
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado mide: " + ladoCuadrado + " cm.");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+} 
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadrado mide: " + perimetroCuadrado + " cm.");
-
-const areaCuadrado = ladoCuadrado*ladoCuadrado;
-console.log("El área del cuadrado mide: " + areaCuadrado + " cm^2.");
+function areaCuadrado(lado) {
+    return lado*lado;
+}
 console.groupEnd();
+
 
 // Código del Triángulo
 console.group("Triángulo");
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
-console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cm, " + ladoTriangulo2 + " cm, " + baseTriangulo + " cm.");
-console.log("El altura del triángulo mide: " + alturaTriangulo + " cm.");
+function perimetroTriangulo(lado1, lado2, base) {
+    if (Number(lado1) == Number(lado2)){
+        return Number(lado1) + Number(lado2) + Number(base);
+    } else {
+        return 0;
+    }
+}
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 +  baseTriangulo;
-console.log("El perimetro del triángulo mide: " + perimetroTriangulo + " cm.");
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo)/0.5;
-console.log("El área del triángulo mide: " + areaTriangulo + " cm^2.");
+function areaTriangulo(lado1, lado2, base) {
+    if (Number(lado1) == Number(lado2)){
+        const altura = Math.sqrt((lado1*lado2) - ((base*base)/4))
+        return (base * altura)/2
+    } else {
+        return 0;
+    };
+}
 console.groupEnd();
 
 // Código del Círculo
 console.group("Círculo");
 
-// Radio
-const radioCirculo = 4;
-console.log("El radio del Círculo mide: " + radioCirculo + " cm.");
+function diametroCirculo(radio) {
+    return radio*2;
+}
 
-// Diámetro 
-const diametroCirculo = radioCirculo*2;
-console.log("El diámetro del Círculo mide: " + diametroCirculo + " cm.");
-
-// PI
 const PI = Math.PI;
-console.log("Pi es = " + PI);
 
-// Circunferencia
-const perimetroCirculo = diametroCirculo*PI;
-console.log("La circunferencia del círculo mide: " + perimetroCirculo + " cm.");
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro*PI;
+}
 
-// Área
-const areaCirculo = (radioCirculo * radioCirculo) * PI;
-console.log("El área del círculo mide: " + areaCirculo + " cm^2.");
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
 
 console.groupEnd();
+
+// Interactuar con HTML
+
+function CalcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function CalcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+function CalcularPerimetroTriangulo() {
+    const input1 = document.getElementById("InputLado1");
+    const input2 = document.getElementById("InputLado2");
+    const inputb = document.getElementById("InputBase");
+    const value1 = input1.value;
+    const value2 = input2.value;
+    const valueb = inputb.value;
+
+    const perimetro = perimetroTriangulo(value1, value2, valueb);
+    if (perimetro == 0){
+        alert("Los lados del triángulo no corresponden a un triángulo isósceles");
+    } else {
+        alert(perimetro);
+    }
+}
+
+function CalcularAreaTriangulo() {
+    const input1 = document.getElementById("InputLado1");
+    const input2 = document.getElementById("InputLado2");
+    const inputb = document.getElementById("InputBase");
+    const value1 = input1.value;
+    const value2 = input2.value;
+    const valueb = inputb.value;
+
+    const area = areaTriangulo(value1, value2, valueb);
+    if (area == 0){
+        alert("Los lados del triángulo no corresponden a un triángulo isósceles");
+    } else {
+        alert(area);
+    }
+}
